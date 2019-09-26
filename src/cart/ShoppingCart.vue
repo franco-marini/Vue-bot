f2620ac17c650c639a3e6f9843c8414a8785107e
Franco Marini <franco-marini@outlook.com>
2019-09-26 15:04:35 -0300

    VB: Using Vuex getters

--- src/cart/ShoppingCart.vue
+++ src/cart/ShoppingCart.vue
@@ -23,6 +23,29 @@
         </tr>
       </tbody>
     </table>
+    <h2 class="saveItems">You saved money on this robots:</h2>
+    <table>
+      <thead>
+        <tr>
+          <th class="robot-title">
+            Robot
+          </th>
+          <th class="cost">
+            Cost
+          </th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="(robot, index) in cartSaleItems" :key="index">
+          <td class="robot-title">
+            {{robot.head.title}}
+          </td>
+          <td class="cost">
+            {{robot.cost}}
+          </td>
+        </tr>
+      </tbody>
+    </table>
  </div>
 </template>
 
@@ -33,6 +56,9 @@ export default {
     cart() {
       return this.$store.state.cart;
     },
+    cartSaleItems(){
+      return this.$store.getters.cartSaleItems;
+    }
   },
 };
 </script>
@@ -47,5 +73,10 @@ export default {
   }
   .cost {
     text-align: right;
+  }
+  .saveItems{
+    margin-top: 50px;
+    font-size: 18px;
+    color: red;
   }
 </style>
