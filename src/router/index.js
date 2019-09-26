ce270337518854c927c9c53ae7ce1f1a112b2548
Franco Marini <franco-marini@outlook.com>
2019-09-26 10:53:40 -0300

    VB: Nested Routes

--- src/router/index.js
+++ src/router/index.js
@@ -4,6 +4,11 @@ import Router from 'vue-router';
 import HomePage from '../home/HomePage.vue';
 import RobotBuilder from '../build/RobotBuilder.vue';
 import PartInfo from '../parts/PartInfo.vue';
+import BrowseParts from '../parts/BrowseParts.vue';
+import RobotArms from '../parts/RobotArms.vue';
+import RobotBases from '../parts/RobotBases.vue';
+import RobotHeads from '../parts/RobotHeads.vue';
+import RobotTorsos from '../parts/RobotTorsos.vue';
 
 Vue.use(Router);
 
@@ -17,6 +22,33 @@ export default new Router({
     path: '/build',
     name: 'Build',
     component: RobotBuilder,
+  },
+  {
+    path: '/parts/browse',
+    name: 'BrowseParts',
+    component: BrowseParts,
+    children: [
+      {
+        name: 'BrowseArms',
+        path: 'arms',
+        component: RobotArms,
+      },
+      {
+        name: 'BrowseBases',
+        path: 'bases',
+        component: RobotBases,
+      },
+      {
+        name: 'BrowseHeads',
+        path: 'heads',
+        component: RobotHeads,
+      },
+      {
+        name: 'BrowseTorsos',
+        path: 'torsos',
+        component: RobotTorsos,
+      },
+    ],
   },
   {
     path: '/parts/:partType/:id',
