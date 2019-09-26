0e09bbfbf0da308c26534976acc78e986c4436b8
Franco Marini <franco-marini@outlook.com>
2019-09-26 11:22:55 -0300

    VB: Named views

---
+++ src/sidebars/SidebarStandard.vue
@@ -0,0 +1,18 @@
+<template>
+ <div class="sidebar">
+   Standar Sidebar
+ </div>
+</template>
+
+<script>
+export default {
+  name: 'Stardard',
+};
+</script>
+
+<style scoped>
+.sidebar{
+  font-size: 50px;
+  transform: rotate(90deg)
+}
+</style>
