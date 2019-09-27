40b6460f1cdb1c741a230796d2bd80462e1b43b0
Franco Marini <franco-marini@outlook.com>
2019-09-27 13:11:59 -0300

    VB: Pasing data with directives

--- src/build/PartSelector.vue
+++ src/build/PartSelector.vue
@@ -10,7 +10,7 @@
     </router-link>
     <button @click="selectPreviousPart()" class="prev-selector"></button>
     <button @click="selectNextPart()" class="next-selector"></button>
-    <span v-pin:position.top.right class="sale" v-show="selectedPart.onSale">Sale!</span>
+    <span v-pin="{ bottom: '10px', right: '5px'  }" class="sale" v-show="selectedPart.onSale">Sale!</span>
   </div>
 </template>
 
