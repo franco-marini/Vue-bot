e30a39948a950e85e1eb39f1c46619b826bab361
Franco Marini <franco-marini@outlook.com>
2019-09-27 13:22:26 -0300

    VB: Directive lifecycle

--- src/build/PartSelector.vue
+++ src/build/PartSelector.vue
@@ -1,5 +1,6 @@
 <template>
   <div class="part" :class="position">
+    {{pinPadding}}
     <router-link :to="{
       name: 'Parts',
       params: {
@@ -10,7 +11,9 @@
     </router-link>
     <button @click="selectPreviousPart()" class="prev-selector"></button>
     <button @click="selectNextPart()" class="next-selector"></button>
-    <span v-pin="{ bottom: '10px', right: '5px'  }" class="sale" v-show="selectedPart.onSale">Sale!</span>
+    <span 
+    @click="pinPadding='30px'"
+    v-pin="{ bottom: pinPadding, right: pinPadding }" class="sale" v-show="selectedPart.onSale">Sale!</span>
   </div>
 </template>
 
@@ -40,7 +43,10 @@ export default {
     },
   },
   data() {
-    return { selectedPartIndex: 0 };
+    return { 
+      selectedPartIndex: 0,
+      pinPadding: '10px',
+      };
   },
   created() {
     this.emitSelectedPart();
